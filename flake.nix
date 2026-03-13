d6af26e839997668cc4e3f9b4d36747163cf54e8
asleepyskye <skye@vixen.lgbt>
2026-03-13 15:41:51 -0400

    trying something?

--- flake.nix
+++ flake.nix
@@ -77,14 +77,6 @@
             path = ./.;
             export = false;
           };
-          # nci.crates."gateway" = {
-          #   depsDrvConfig.mkDerivation = {
-          #     nativeBuildInputs = [ pkgs.protobuf ];
-          #   };
-          #   drvConfig.mkDerivation = {
-          #     nativeBuildInputs = [ pkgs.protobuf ];
-          #   };
-          # };
 
           packages =
             let
@@ -99,6 +91,19 @@
                 "scheduled_tasks"
               ];
               binaries = lib.genAttrs services (name: rustOutputs.${name}.packages.release);
+
+              nci.crates = lib.genAttrs services (name: {
+                depsDrvConfig.mkDerivation = {
+                  nativeBuildInputs = [ pkgs.protobuf ];
+                  preConfigure = ''
+                    find /build/nix-vendor -name Cargo.toml -exec sed -i '/\[lints\]/d; /workspace = true/d' {} + || true
+                  '';
+                };
+                drvConfig.mkDerivation = {
+                  nativeBuildInputs = [ pkgs.protobuf ];
+                };
+              });
+
               dockerImages = lib.genAttrs services (
                 name:
                 pkgs.dockerTools.streamLayeredImage {
